27d023eaf4aa0f2a035b290bb17a6d3b689da287
Code2Craft <cristiangidu01@gmail.com>
2025-04-06 12:59:37 +0300

    Created

---
+++ multiplicaton-of-2-numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"The multiplication of 2 numbers"<<endl;
+    cout<<"---------------------"<<endl;
+
+    int a, b, res=0;
+
+    cout<<"Enter the first number: ";
+    cin>>a;
+
+    cout<<"Enter the second number: ";
+    cin>>b;
+
+    sum = a * b;
+
+    cout<<"The multiplication of "<<a<<" and "<<b<<" is "<<res<<endl;
+
+    return 0;
+}

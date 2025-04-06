b9508de01225dc3c4c2862bba366e8ff3fe2bccd
Code2Craft <cristiangidu01@gmail.com>
2025-04-06 12:49:05 +0300

    Create difference-of-2-numbers.cpp

---
+++ difference-of-2-numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"The difference of 2 numbers"<<endl;
+    cout<<"---------------------"<<endl;
+
+    int a, b, sum=0;
+
+    cout<<"Enter the first number: ";
+    cin>>a;
+
+    cout<<"Enter the second number: ";
+    cin>>b;
+
+    sum = a - b;
+
+    cout<<"The difference of "<<a<<" and "<<b<<" is "<<sum<<endl;
+
+    return 0;
+}
